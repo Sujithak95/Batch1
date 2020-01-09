aff0efd96e287b1febf20e5c2517fd358d396dd5
Priyadharshnib <priyab4ucool@gmail.com>
2020-01-09 13:12:22 +0530

    Modified Basepage

--- src/main/java/Automation/Basepage.java
+++ src/main/java/Automation/Basepage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Basepage {
+public class Basepage1 {
 
 	public static WebDriver driver;
 	public String browser = "chrome";
